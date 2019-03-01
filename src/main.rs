c84b1f67c8848470594b3eabaafd5eb6ef88a5c8
Chris Paterson <cjpaterson.3@gmail.com>
2019-02-28 19:21:46 -0500

    Add config struct, test.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use std::env;
+use std::process;
+
+pub struct Config {
+    pub arguments: Vec<String>,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 2 {
+            return Err("Not enough arguments.");
+        }
+
+        let mut arguments = Vec::with_capacity(args.len() - 1);
+        arguments.clone_from_slice(&args[1..]);
+        Ok(Config { arguments })
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn not_enough_args() {
+        let args: [String; 1] = [String::from("recall")];
+
+        match Config::new(&args) {
+            Ok(_) => assert!(false, "Config should require at least two arguments."),
+            Err(_) => assert!(true),
+        }
+    }
 }
